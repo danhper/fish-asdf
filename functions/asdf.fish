dca67552b9447bfecef3e37874a329f3bae9a9f3
Daniel Perez <daniel@claudetech.com>
2016-04-19 12:04:38 +0900

    Improve global and local commands to accept a single argument.

--- functions/asdf.fish
+++ functions/asdf.fish
@@ -1,19 +1,9 @@
-function __asdf_check_args
-	if test (count $argv) -lt 3
-		echo "global needs a plugin and a version"
+function __asdf_set_version -a file -a plugin -a language_version
+	if not contains $language_version (command asdf list $plugin)
+		echo $plugin does not have version $language_version
 		return 1
 	end
-	if not contains $argv[2] (command asdf plugin-list)
-		echo plugin $argv[2] does not exist
-		return 1
-	end
-	if not contains $argv[3] (command asdf list $argv[2])
-		echo $argv[2] does not have version $argv[3]
-		return 1
-	end
-end
 
-function __asdf_set_version -a plugin -a language_version -a file
 	if test -f $file; and grep $plugin $file > /dev/null
 		sed -i -e "s/$plugin .*/$plugin $language_version/" $file
 	else
@@ -21,17 +11,55 @@ function __asdf_set_version -a plugin -a language_version -a file
 	end
 end
 
-function asdf -a cmd
+function __asdf_get_version -a file -a plugin
+	if not test -f $file
+		echo $file does not exist
+		return 1
+	end
+	if not grep --color=never $plugin $file
+		echo $plugin version is not set in $file
+	end
+end
+
+function __asdf_version -a cmd
+	set -l argc (count $argv)
+
+	if test $argc -ne 2 -a $argc -ne 3
+		echo usage: $cmd PLUGIN [VERSION]
+		return 1
+	end
+
+	set -l plugin $argv[2]
+
+	set -l file
 	switch $cmd
 		case "global"
-			if __asdf_check_args $argv
-				__asdf_set_version $argv[2] $argv[3] ~/.tool-versions
-			end
+			set file ~/.tool-versions
 		case "local"
-			if __asdf_check_args $argv
-				__asdf_set_version $argv[2] $argv[3] (pwd)/.tool-versions
-			end
-	case "*"
-			command asdf $argv
+			set file .tool-versions
+	end
+
+	if not contains $plugin (command asdf plugin-list)
+		echo plugin $plugin does not exist
+		return 1
+	end
+
+	if test $argc -eq 2
+		__asdf_get_version $file $plugin
+	else
+		__asdf_set_version $file $plugin $argv[3]
+	end
+end
+
+function asdf
+	if test (count $argv) -eq 0
+		command asdf
+	else
+		switch $argv[1]
+			case "global" "local"
+				__asdf_version $argv
+			case "*"
+				command asdf $argv
+		end
 	end
 end
